c515a2a01e7a8738bcb0f8ded5f575662056ca74
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-02-15 20:10:31 -0500

    funcion suma

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-function showNumber(number){
-  console.log(`El numero ingresado es ${number}`)
+function suma(a,b){
+  console.log (`La suma de ${a} mas ${b} es ${a+b}`)
 }
-
+suma(10,84)
 
 function resta(a,b){
   console.log (`La resta de ${a} menos ${b} es ${a-b}`)
