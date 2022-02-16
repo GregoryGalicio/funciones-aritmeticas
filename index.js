d051b0d6a1a790ca237e399d7ec8465a94d2ae8e
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-02-15 19:45:35 -0500

    funcion resta

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function showNumber(number){
+  console.log(`El numero ingresado es ${number}`)
+}
+
+
+function resta(a,b){
+  console.log (`La resta de ${a} menos ${b} es ${a-b}`)
+}
+
+resta(15,5)
