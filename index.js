afdea138286cf78fc2f600ff915605e0cefca58f
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-03-15 18:31:52 -0500

    funcion resta1

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ multiplicacion(10,5)
 const division=(a,b)=> console.log (`La division de ${a} entre ${b} es ${a/b}`)
 
 multiplicacion(100,25)
+
+const resta1=(a,b)=> console.log(`La resta de ${a} menos ${b} es ${a-b}`)
+resta1(40,3)
